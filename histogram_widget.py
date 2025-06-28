6fad448bac98a06eb2ebc4278dcbc1ed4d1c16c4
uniuyuni <uniuyuni@gmail.com>
2025-06-28 09:20:41 +0900

    Add Histogram rect

--- histogram_widget.py
+++ histogram_widget.py
@@ -23,27 +23,32 @@ class HistogramWidget(KVImage):
         #r_hist, g_hist, b_hist = [np.histogram(pixels[..., i], bins=256, range=(0, 1.0))[0] for i in range(3)]
         r_hist, g_hist, b_hist = [cv2.calcHist([pixels], [i], None, [256], [0, 2.0]) for i in range(3)]
         r_hist[0] = max(0, r_hist[0] - exclude_zero_count)
+        #r_hist = np.clip(r_hist, 0, 255)
         g_hist[0] = max(0, g_hist[0] - exclude_zero_count)
+        #g_hist = np.clip(g_hist, 0, 255)
         b_hist[0] = max(0, b_hist[0] - exclude_zero_count)
+        #b_hist = np.clip(b_hist, 0, 255)
         
         # 輝度の計算
         luminance = cv2.cvtColor(pixels, cv2.COLOR_RGB2GRAY)
         #l_hist, _ = np.histogram(luminance, bins=256, range=(0, 1.0))
         l_hist = cv2.calcHist([luminance], [0], None, [256], [0, 2.0])
         l_hist[0] = max(0, l_hist[0] - exclude_zero_count)
+        #l_hist = np.clip(l_hist, 0, 255)
 
         # ヒストグラムを描画
         self.canvas.clear()
         with self.canvas:
             KVColor((0.8, 0.8, 0.8, 1))
-            KVLine(rectangle=(self.pos[0], self.pos[1], dp(256), dp(100)), width=1)
+            KVLine(rectangle=(self.pos[0], self.pos[1], 256, 256), width=1)
+            KVLine(rectangle=(self.pos[0]+256, self.pos[1], 256, 256), width=1)
         self.__draw_histogram_bars(r_hist, (1, 0, 0, 0.8))
         self.__draw_histogram_bars(g_hist, (0, 1, 0, 0.8))
         self.__draw_histogram_bars(b_hist, (0, 0, 1, 0.8))
         self.__draw_histogram_bars(l_hist, (0.8, 0.8, 0.8, 1))  # 輝度ヒストグラムをグレーで表示
 
     def __draw_histogram_bars(self, histogram, color, offset_x=0, offset_y=0):
-        max_value = np.max(histogram[1:255])
+        max_value = np.max(histogram)
         bar_width = 2
         offset_x += self.pos[0]
         offset_y += self.pos[1]
@@ -52,7 +57,7 @@ class HistogramWidget(KVImage):
             KVPushMatrix()
             KVColor(*color)
             for x, value in enumerate(histogram):
-                height = min(dp(100), (value[0] / max_value) * dp(100))  # ヒストグラムの高さを設定
+                height = min(255, (value[0] / max_value) * 255)  # ヒストグラムの高さを設定
                 KVRectangle(pos=(x * bar_width + offset_x, offset_y), size=(bar_width, height))
             KVPopMatrix()
 
